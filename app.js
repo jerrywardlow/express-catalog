baec9a57fb657ce835c2ed46d1c6fd1a653250c3
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-04-22 23:18:42 -0700

    Updated routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var item = require('./routes/item');
+var category = require('./routes/category');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/item', item);
+app.use('/category', category);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
