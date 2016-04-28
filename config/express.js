a40b0d1f7ffa7133ecb4c0a3ea63f04af28df1a4
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-04-27 23:18:46 -0700

    Using 'pug' engine for views

--- config/express.js
+++ config/express.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 module.exports = function() {
     var app = express();
+
+    app.set('views', './app/views');
+    app.set('view engine', 'pug')
+
     require('../app/routes/index.app.routes.js')(app);
     return app;
 };
