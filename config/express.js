16e93ae488ee87d3457a2b513efdd5101bb4b820
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-05-04 20:21:23 -0700

    Fixed views definition

--- config/express.js
+++ config/express.js
@@ -3,8 +3,8 @@ var express = require('express');
 module.exports = function() {
     var app = express();
 
-    app.set('views', './app/views');
-    app.set('view engine', 'pug')
+    app.set('views', __dirname + '/../app/views');
+    app.set('view engine', 'pug');
 
     require('../app/routes/index.app.routes.js')(app);
 
