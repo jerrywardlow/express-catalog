41639e4f0021435d51a0cf338ba8892d241d59ef
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-20 15:48:03 -0700

    Add morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan("short"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
